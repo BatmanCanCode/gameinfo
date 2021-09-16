b63f32ca8c0c0e8dc33105eda47718801e46920e
420 <atkinsoncam9@gmail.com>
2021-09-17 00:19:56 +0100

    Update 0.1
    New player system using cam not a person

--- Assets/scripts/prop_chair.cs
+++ Assets/scripts/prop_chair.cs
@@ -4,27 +4,7 @@ using UnityEngine;
 
 public class prop_chair : MonoBehaviour
 {
-    public bool seat = false;
-    public GameObject player;
-    public Animator animator;
-    public GameObject seat_1;
-    public GameObject seat_1_mark;
 
-    public bool sat;
-    void OnTriggerStay(Collider other)
-     {
-         Rigidbody h = player.GetComponent<Rigidbody>();
-         if(!sat)
-         {
-          if (Input.GetKeyDown(KeyCode.E))
-          {
-            
-            animator.Play("sat");
-          }
-         }
-          
-     } //transform.rotation.y = target.rotation.y;   1.164  0.581 -0.7182184
-    
 }
 
 
